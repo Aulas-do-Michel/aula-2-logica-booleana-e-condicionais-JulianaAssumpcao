e71b59d412d7653ff763a2bac14944d2c1af0872
JulianaAssumpcao <163673881+JulianaAssumpcao@users.noreply.github.com>
2024-03-26 10:55:57 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,19 +1,7 @@
-"""
-#### Exercício 1
-
-Receba um número inteiro de um usuário. Se ele for par, imprima "Par". Se não, imprima "Ímpar".
-
-Exemplo:
-
-Digite um número:
-10
-
-Par
---------
-Digite um número:
-1
-
-Ímpar
-
-Dica: Lembre do comando de resto da divisão inteira!
-"""
+#Exercício 1 Receba um número inteiro de um usuário. Se ele for par, imprima "Par". Se não, imprima "Ímpar".
+preco=int(input("Digite o preco: "))
+resto=preco%2
+if resto==0:
+  print("Par")
+else:
+  print("Impar")
